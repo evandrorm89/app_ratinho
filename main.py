1c663a37a5ab7f8522c1e945a9ec6eed2b5842c6
Evandro Magalhaes <evandrorm89@gmail.com>
2020-07-20 23:48:22 -0300

    versao 0.2

--- main.py
+++ main.py
@@ -10,25 +10,29 @@ from kivy.config import Config
       
 # 0 being off 1 being on as in true / false  
 # you can use 0 or 1 && True or False  
-Config.set('graphics', 'resizable', True) 
+Config.set('graphics', 'resizable', True)
+Config.set('kivy','window_icon','images/hqdefault.ico')
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.scrollview import ScrollView
 from kivy.core.audio import SoundLoader
+from kivy.properties import ObjectProperty
 
 Builder.load_file('design.kv')
 
 class InitialScreen(Screen):
 
-    def play(self, sound):
-        print('soundsss')
-        path = ('sounds/{}.wav'.format(sound))
-        sound = SoundLoader.load(path)
-        if sound:
-            print("Sound found at %s" % sound.source)
-            print("Sound is %.3f seconds" % sound.length)
-            sound.play()
+    sound = ObjectProperty(None, allownone=True)
+
+    def play(self, filename):
+        if self.sound is None:
+            path = ('sounds/{}.wav'.format(filename))
+            self.sound = SoundLoader.load(path)
+        #stop sound if it's playing
+        if self.sound.status != 'stop':
+            self.sound.stop()
+        self.sound.play()
 
 
 
